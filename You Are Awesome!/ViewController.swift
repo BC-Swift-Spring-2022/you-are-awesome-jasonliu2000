45d96b428798c7bc7a41fb5fda4bc927b651f398
Jason Liu <jasonliu00@gmail.com>
2022-01-31 15:59:29 -0500

    Section 2.9 — Add image cycle

--- You Are Awesome!/ViewController.swift
+++ You Are Awesome!/ViewController.swift
@@ -12,25 +12,36 @@ class ViewController: UIViewController {
     @IBOutlet weak var imageView: UIImageView!
     @IBOutlet weak var messageLabel: UILabel!
     
+    var imageNumber = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
 
     @IBAction func messageButtonPressed(_ sender: UIButton) {
-        let awesomeMessage = "You Are Awesome!"
-        let greatMessage = "You Are Great!"
-        let bombMessage = "You Are Da Bomb!"
         
-        if messageLabel.text == awesomeMessage {
-            messageLabel.text = greatMessage
-            imageView.image = UIImage(named: "image1")
-        } else if messageLabel.text == greatMessage {
-            messageLabel.text = bombMessage
-            imageView.image = UIImage(named: "image2")
+        let imageName = "image\(imageNumber)"
+        imageView.image = UIImage(named: imageName)
+        if imageNumber == 9 {
+            imageNumber = 0
         } else {
-            messageLabel.text = awesomeMessage
-            imageView.image = UIImage(named: "image0")
+            imageNumber += 1
         }
+        
+//        let awesomeMessage = "You Are Awesome!"
+//        let greatMessage = "You Are Great!"
+//        let bombMessage = "You Are Da Bomb!"
+//
+//        if messageLabel.text == awesomeMessage {
+//            messageLabel.text = greatMessage
+//            imageView.image = UIImage(named: "image1")
+//        } else if messageLabel.text == greatMessage {
+//            messageLabel.text = bombMessage
+//            imageView.image = UIImage(named: "image2")
+//        } else {
+//            messageLabel.text = awesomeMessage
+//            imageView.image = UIImage(named: "image0")
+//        }
 
     }
     
